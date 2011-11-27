88e838f327de4c4b7aa03bea683c414a710a5307
andrew morton <drewish@katherinehouse.com>
2011-11-27 12:04:39 -0500

    Initial Commit

---
+++ munchem/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  munchem
+//
+//  Created by andrew morton on 11/27/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ munchem/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  munchem
+//
+//  Created by andrew morton on 11/27/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
